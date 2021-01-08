0463b31486517771a75820c36b33010bba366e48
Suchir Kavi <suchirkavi@gmail.com>
2021-01-08 17:53:41 -0600

    Day 1

---
+++ 2020/day1/src/main.rs
@@ -0,0 +1,21 @@
+use std::io::{self, Read};
+use std::collections::HashSet;
+fn main() -> io::Result<()> {
+    const YEAR: i32 = 2020;
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+    let report = buffer.lines()
+        .filter_map(|s| s.parse::<i32>().ok())
+        .map(|entry| if entry > YEAR / 2 {YEAR - entry} else {entry});
+    
+    let mut visited = HashSet::new();
+
+    for entry in report {
+        if !visited.insert(entry) {
+            println!("{}", entry * (YEAR - entry));
+            break;
+        }
+    }
+
+    Ok(())
+}
