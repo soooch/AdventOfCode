1bff53adddfbb929dca9a5ba504db2b18dc69c89
Suchir Kavi <suchirkavi@gmail.com>
2021-01-09 01:10:11 -0600

    why can't I figure out gitignore

--- 2020/day1/part2/target/debug/day1_part2.d
+++
@@ -1,1 +0,0 @@
-/Users/soooch/Develop/AoC/2020/day1/part2/target/debug/day1_part2: /Users/soooch/Develop/AoC/2020/day1/part2/src/main.rs
--- 2020/day1/part2/target/debug/part2.d
+++
@@ -1,1 +0,0 @@
-/Users/soooch/Develop/AoC/2020/day1/part2/target/debug/part2: /Users/soooch/Develop/AoC/2020/day1/part2/src/main.rs
