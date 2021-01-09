cc0468bdc4f5b0a94d238e8df0ac118df11a6b63
Suchir Kavi <suchirkavi@gmail.com>
2021-01-08 18:03:24 -0600

    untrack target

--- 2020/day1/part1/target/debug/deps/Day1.d
+++
@@ -1,5 +0,0 @@
-/Users/soooch/Develop/AoC/2020/day1/target/debug/deps/day1: src/main.rs
-
-/Users/soooch/Develop/AoC/2020/day1/target/debug/deps/day1.d: src/main.rs
-
-src/main.rs:
--- 2020/day1/part1/target/debug/deps/day1-e3f74a22fd0740f3.d
+++
@@ -1,5 +0,0 @@
-/Users/soooch/Develop/AoC/2020/day1/target/debug/deps/day1-e3f74a22fd0740f3.rmeta: src/main.rs
-
-/Users/soooch/Develop/AoC/2020/day1/target/debug/deps/day1-e3f74a22fd0740f3.d: src/main.rs
-
-src/main.rs:
