192183de3dfba7c5ac88638e0f05eac0a096688e
Suchir Kavi <suchirkavi@gmail.com>
2021-01-13 15:55:54 -0600

    cleaned up the first bit. mostly just to see if I have git working again on this computer

--- 2020/day1/part1/src/main.rs
+++ 2020/day1/part1/src/main.rs
@@ -4,17 +4,18 @@ fn main() -> io::Result<()> {
     const YEAR: i32 = 2020;
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
+    let mut visited = HashSet::new();
     let report = buffer.lines()
         .filter_map(|s| s.parse::<i32>().ok())
-        .map(|entry| if entry < YEAR / 2 {YEAR - entry} else {entry});
+        .map(|entry| if entry < YEAR / 2 {YEAR - entry} else {entry})
+        .find(|&entry| !visited.insert(entry))
+        .map(|entry| entry * (YEAR - entry));
     
-    let mut visited = HashSet::new();
-
-    for entry in report {
-        if !visited.insert(entry) {
-            println!("{}", entry * (YEAR - entry));
-            break;
-        }
+    if let Some(answer) = report {
+       println!("{}", answer);
+    }
+    else {
+       println!("could not find two numbers that add to {}", YEAR);
     }
 
     Ok(())
