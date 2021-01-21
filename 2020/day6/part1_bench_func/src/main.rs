81faf354d8fe4d102af2a8e7c0a332d984683d3f
Suchir Kavi <suchirkavi@gmail.com>
2021-01-21 14:33:05 -0600

    d6p1 updated

--- 2020/day6/part1_bench_func/src/main.rs
+++ 2020/day6/part1_bench_func/src/main.rs
@@ -1,20 +1,25 @@
 use std::io::{self, Read};
+use std::time::Instant;
 
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
+    
+    let now = Instant::now();
 
     let sum = buffer.split("\n\n")
         .fold(0, |acc, group| {
             let mut yesses: u32 = 0;
             group.chars().for_each(|q| {
                 match q {
-                    'a'..='z' => yesses |= 1 << (q as u32 - 'a' as u32),
+                    'a'..='z' => yesses |= 1 << (q as u8 - 'a' as u8),
                     _ => (),
                 }
             });
             acc + yesses.count_ones()
         });
+
+    println!("{}", now.elapsed().as_millis());
 
     println!("{}", sum);
 
