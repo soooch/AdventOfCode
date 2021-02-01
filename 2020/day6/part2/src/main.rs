25e327223df5cb5e350c9ef821928ffe8e720bf6
Suchir Kavi <suchirkavi@gmail.com>
2021-01-31 20:00:10 -0600

    made d6p2 much cleaner

--- 2020/day6/part2/src/main.rs
+++ 2020/day6/part2/src/main.rs
@@ -1,21 +1,23 @@
 use std::io::{self, Read};
+use std::time::Instant;
 
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
 
-    let sum = buffer.split("\n\n")
-        .fold(0, |acc, group| {
-            let all_yesses: u32 = group.lines()
-                .fold(u32::MAX, |all_yes_acc, line| {
-                    let mut line_yesses: u32 = 0;
-                    line.bytes().for_each(|q| {
-                        line_yesses |= 1 << (q - b'a');
-                    });
-                    all_yes_acc & line_yesses
-                });
-            acc + all_yesses.count_ones()
-        });
+    let now = Instant::now();
+    let sum: u32 = buffer
+        .split("\n\n")
+        .map(|group| {
+            group
+                .lines()
+                .map(|line| line.bytes().fold(0, |map, q| map | 1 << (q - b'a')))
+                .fold(u32::MAX, |map, line| map & line)
+                .count_ones()
+        })
+        .sum();
+
+    println!("{}", now.elapsed().as_millis());
 
     println!("{}", sum);
 
